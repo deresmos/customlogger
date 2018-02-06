5edeb3114be8cd4dbbdbeb17cc4aa96d33af21c6
deresmos <deresmos@gmail.com>
2018-02-06 17:12:30 +0900

    Change 0.2.7 version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ __author__ = 'deresmos'
 
 setup(
     name='customlogger',
-    version='0.2.6',
+    version='0.2.7',
     description='custom logger class.',
     author='deresmos',
     author_email='deresmos@gmail.com',
